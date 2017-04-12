e98b435d7d06416c777d9ab19f1a8dad67882d8b
caoxiaolin <xmihu945@qq.com>
2017-04-12 12:46:26 +0800

    modify by caoxiaolin

--- shorturl.lua
+++ shorturl.lua
@@ -1,12 +1,19 @@
-local short = require "short"
+local short = require "utils.short"
 
 local method = ngx.req.get_method()
+
+-- http get, return original url
 if method == "GET" then
     short:getUrl(ngx.var.uri)
+-- http post, create a short url
 elseif method == "POST" then
     ngx.req.read_body()
     local url = ngx.req.get_post_args()["url"] or ""
-    short:setUrl(url)
+    if url ~= "" then
+        short:setUrl(url)
+    else
+        ngx.say("please post url")
+    end
 else
     ngx.say("error")
 end
